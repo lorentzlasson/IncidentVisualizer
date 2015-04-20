72b18f5ac72651285592df2596324e1517d750cb
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-04-20 23:30:47 +0200

    reads credentials from config file

--- mqtt.go
+++ mqtt.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
 	"os"
 )
+
+type Configuration struct {
+	Org      string
+	Username string
+	Password string
+}
+
+var credentials = Configuration{}
 
 // define a function for the default message handler
 var f MQTT.MessageHandler = func(client *MQTT.MqttClient, msg MQTT.Message) {
@@ -13,13 +22,25 @@ var f MQTT.MessageHandler = func(client *MQTT.MqttClient, msg MQTT.Message) {
 	h.broadcast <- msg.Payload()
 }
 
+func readCredentials() {
+	file, _ := os.Open("iotfcreds.json")
+	decoder := json.NewDecoder(file)
+	err := decoder.Decode(&credentials)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+}
+
 func initMqtt() {
+	readCredentials()
+	broker := fmt.Sprintf("tcp://%s.messaging.internetofthings.ibmcloud.com:1883", credentials.Org)
+	opts := MQTT.NewClientOptions().AddBroker(broker)
+	opts.SetUsername(credentials.Username)
+	opts.SetPassword(credentials.Password)
+	clientId := fmt.Sprintf("a:%s:%s", credentials.Org, "visualizer")
+	opts.SetClientId(clientId)
 
-	opts := MQTT.NewClientOptions().AddBroker("tcp://qchp0k.messaging.internetofthings.ibmcloud.com:1883")
-	opts.SetUsername("a-qchp0k-s4ywm4ruua")
-	opts.SetPassword("0MaeP8nAIRfYiT!-ub")
-	opts.SetClientId("a:qchp0k:visualizer")
-
+	fmt.Print("Connecting to mqtt...")
 	//create and start a client using the above ClientOptions
 	c := MQTT.NewClient(opts)
 	_, err := c.Start()
@@ -27,6 +48,7 @@ func initMqtt() {
 		panic(err)
 	}
 
+	fmt.Println(" success!")
 	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
 	//at a maximum qos of zero, wait for the receipt to confirm the subscription
 	topic, err := MQTT.NewTopicFilter("iot-2/type/+/id/+/evt/incident/fmt/json", 0)
